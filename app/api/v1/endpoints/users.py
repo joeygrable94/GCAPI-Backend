a5747ea9707dc5e145247267058ae35ff3210073
Joey Grable <hello@joeygrable.com>
2022-05-28 21:36:18 -0700

    updated users endpoints

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,10 +1,9 @@
 from typing import List
 from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi_users import BaseUserManager
 from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.core.user_manager import current_active_super_user
+from app.core.user_manager import current_active_super_user, get_user_manager
 from app.db.schemas import UserRead
-from app.api.deps import get_user_db
 
 
 users_router = APIRouter()
@@ -16,14 +15,14 @@ users_router = APIRouter()
     name="users:read_users"
 )
 async def read_users(
-    user_db: AsyncSession = Depends(get_user_db),
+    user_manager: BaseUserManager = Depends(get_user_manager),
     skip: int = 0,
     limit: int = 10,
     current_super_user: UserRead = Depends(current_active_super_user),
 ):
     try:
-        statement = select(user_db.user_table).limit(limit).offset(skip)
-        result = await user_db.session.execute(statement)
+        statement = select(user_manager.user_db.user_table).limit(limit).offset(skip)
+        result = await user_manager.user_db.session.execute(statement)
         return result.scalars().all()
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
