a0bdd95738844603d2e7a37967cd2ba2dd502531
Joey Grable <hello@joeygrable.com>
2022-07-22 11:05:18 -0800

    switched custom GUID to SQLA-utils UUIDType

--- app/db/tables/base.py
+++ app/db/tables/base.py
@@ -1,20 +1,22 @@
 from datetime import datetime
 from typing import Any
 
-from sqlalchemy import CHAR, Column, DateTime
+from sqlalchemy import CHAR, Column, DateTime, func
+from sqlalchemy_utils import UUIDType
 
 from app.db.base_class import Base, BaseMixin
-from app.db.types import GUID
-from app.db.utilities import _get_date, _get_uuid
+from app.db.utilities import _get_uuid
 
 
 class TableBase(BaseMixin, Base):
     __abstract__: bool = True
     __mapper_args__: Any = {"always_refresh": True}
-    id: Column[str] = Column(
-        GUID(), primary_key=True, unique=True, nullable=False, default=lambda: str(_get_uuid())
+    id: Column[Any] = Column(
+        UUIDType(binary=False), primary_key=True, unique=True, nullable=False, default=_get_uuid()
     )
-    created_on: Column[datetime] = Column(DateTime(timezone=True), default=_get_date)
+    created_on: Column[datetime] = Column(DateTime(timezone=True), default=func.current_timestamp())
     updated_on: Column[datetime] = Column(
-        DateTime(timezone=True), default=_get_date, onupdate=_get_date
+        DateTime(timezone=True),
+        default=func.current_timestamp(),
+        onupdate=func.current_timestamp()
     )
