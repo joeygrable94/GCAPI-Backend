b1cde212eb4cfeb72efba474263631f0b15d4de0
Joey Grable <hello@joeygrable.com>
2022-07-25 09:03:55 -0700

    configured pyproject.toml and refactored format.sh and lint.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,5 +1,10 @@
-#!/bin/sh -e
+#!/usr/bin/env bash
+
+set -e
 set -x
+
+# Sort imports one per line, so autoflake can remove unused imports
+isort --force-single-line-imports app
 
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude=__init__.py
 black app
