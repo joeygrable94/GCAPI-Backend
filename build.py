e643a41f06a6ad9392e6128e984d4a91a03a83a7
Joey Grable <hello@joeygrable.com>
2023-01-22 13:58:11 -0800

    added build.py for first time startup, and refactored prestart for only rebuilding the backend server

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
 import asyncio
 
+from app.core.config import settings
+from app.db.init_db import build_database, create_init_data
 from app.db.commands import check_db_connected
 from app.core.logger import logger  # pragma: no cover
 
 
 async def main() -> None:  # pragma: no cover
     await check_db_connected()
+    if settings.DEBUG_MODE:
+        await build_database()
+    await create_init_data()
 
 
 if __name__ == "__main__":  # pragma: no cover
