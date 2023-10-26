673f8e2ed11cbef7b7213beda1b7769943399794
Joey Grable <hello@joeygrable.com>
2023-10-26 13:11:06 -0700

    refactored base class

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -2,4 +2,8 @@ from typing import Any
 
 from sqlalchemy.orm import declarative_base
 
-Base: Any = declarative_base()
+declared_base: Any = declarative_base()
+
+
+class Base(declared_base):
+    pass
