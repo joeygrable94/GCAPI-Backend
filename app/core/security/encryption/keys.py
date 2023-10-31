c11a656ee90f1a0b60c3adf7eabbf39c85ea0c20
Joey Grable <hello@joeygrable.com>
2023-10-30 20:26:13 -0700

    fixed rsa keys to use 2048 bytes

--- app/core/security/encryption/keys.py
+++ app/core/security/encryption/keys.py
@@ -7,27 +7,29 @@ from app.core.config import settings
 
 
 def generate_api_keys(file_key: str = settings.api.key) -> None:
-    (pub_key, pvt_key) = rsa.newkeys(512)
+    (pub_key, pvt_key) = rsa.newkeys(2048)
     private_pem = pub_key.save_pkcs1().decode()
     public_pem = pvt_key.save_pkcs1().decode()
-    with open(f"./certs/{file_key}_private_key.pem", "w") as pr:
+    pvt_pem_file = f"{settings.api.root_dir}/{file_key}_private_key.pem"
+    pub_pem_file = f"{settings.api.root_dir}/{file_key}_public_key.pem"
+    with open(pvt_pem_file, "w") as pr:
         pr.write(private_pem)
-    with open(f"./certs/{file_key}_public_key.pem", "w") as pu:
+    with open(pub_pem_file, "w") as pu:
         pu.write(public_pem)
 
 
 def load_api_keys(
     file_key: str = settings.api.key,
 ) -> Tuple[rsa.PrivateKey, rsa.PublicKey]:
-    pub_pem_file = f"{settings.api.root_dir}/{file_key}_private_key.pem"
-    pvt_pem_file = f"{settings.api.root_dir}/{file_key}_public_key.pem"
-    if not os.path.exists(pub_pem_file) and not os.path.exists(pvt_pem_file):
+    pvt_pem_file = f"{settings.api.root_dir}/{file_key}_private_key.pem"
+    pub_pem_file = f"{settings.api.root_dir}/{file_key}_public_key.pem"
+    if not os.path.exists(pvt_pem_file) and not os.path.exists(pub_pem_file):
         generate_api_keys(settings.api.key)
         load_api_keys(settings.api.key)
-    with open(pub_pem_file, "r") as pu:
-        public_pem = pu.read().encode("utf-8")
     with open(pvt_pem_file, "r") as pr:
         private_pem = pr.read().encode("utf-8")
-    pub_key = rsa.PublicKey.load_pkcs1(public_pem)
+    with open(pub_pem_file, "r") as pu:
+        public_pem = pu.read().encode("utf-8")
     pvt_key = rsa.PrivateKey.load_pkcs1(private_pem)
+    pub_key = rsa.PublicKey.load_pkcs1(public_pem)
     return pvt_key, pub_key
--- build.py
+++ build.py
@@ -1,10 +1,12 @@
 import asyncio
 
+from app.core.security.encryption.keys import generate_api_keys
 from app.db.commands import check_db_connected, build_database, create_init_data
 from app.core.logger import logger  # pragma: no cover
 
 
 async def main() -> None:  # pragma: no cover
+    generate_api_keys()
     await check_db_connected()
     await build_database()
     await create_init_data()
