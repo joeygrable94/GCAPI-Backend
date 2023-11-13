381737494625dcc03f0fab5c9a7af2ae51a6b5cf
Joey Grable <hello@joeygrable.com>
2023-11-13 09:41:39 -0800

    improved the cipher security
    - added password-salt derived aes_key
    - changed to AES_CBC mode with IV

--- app/api/deps/get_encryption.py
+++ app/api/deps/get_encryption.py
@@ -9,7 +9,10 @@ from app.core.security import SecureMessage
 def get_secure_message_encryption(
     settings: Settings = Depends(get_settings),
 ) -> SecureMessage:  # pragma: no cover
-    return SecureMessage(aes_key=settings.api.encryption_key)
+    return SecureMessage(
+        pass_key=settings.api.encryption_key,
+        salt=settings.api.encryption_salt,
+    )
 
 
 SecureMessageEncryption = Annotated[
