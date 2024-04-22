bf242305f60d54f5b4cb5ce6d2675a62e4f6e8aa
Joey Grable <hello@joeygrable.com>
2024-04-22 11:43:55 -0700

    updated package dependencies

--- app/db/custom_types/__init__.py
+++ app/db/custom_types/__init__.py
@@ -1,11 +1,7 @@
-# from .encrypted_scopes import EncryptedScopes
-# from .encrypted_string import EncryptedString
 from .longtext import LongText
 from .scopes import Scopes
 
 __all__: list[str] = [
-    # "EncryptedScopes",
-    # "EncryptedString",
     "LongText",
     "Scopes",
 ]
